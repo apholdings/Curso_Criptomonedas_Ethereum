5ae4b25672e28fe16f0b8db46026ed14858e6431
SoloPython <alexander.eric.3@gmail.com>
2023-08-01 12:47:12 -0500

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,14 @@ module.exports = {
     solidity: "0.8.9",
 
     networks: {
-
+        goerli: {
+            url: process.env.ETHEREUM_TESTNET_RPC,
+            accounts: [process.env.PRIVATE_KEY]
+        }
     },
 
     etherscan: {
-
+        apiKey: process.env.POLYGONSCAN_API_KEY,
     },
 
     paths: {
